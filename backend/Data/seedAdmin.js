63b4fb7da03ef7758e4ebbdf75a09ca665c8e980
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-07-05 16:11:57 +0530

    Add admin to db using seed file

---
+++ backend/Data/seedAdmin.js
@@ -0,0 +1,30 @@
+import Admin from "../models/createUser.js";
+import bcrypt from "bcrypt";
+
+const temPasswordHash = await bcrypt.hash("12345678", 12);
+
+const Admins = [
+  {
+    name: "Sanjula",
+    email: "sdulshan123@gmail.com",
+    accountType: "admin",
+    password: temPasswordHash,
+  },
+  {
+    name: "Dilsha",
+    email: "dilsha99t@gmail.com",
+    accountType: "student",
+    password: temPasswordHash,
+  },
+];
+
+const seedAdmin = async () => {
+  try {
+    await Admin.create(Admins);
+    console.log("Admin Created...");
+  } catch (err) {
+    console.log("Admin Creation Failed...", err.message);
+  }
+};
+
+export default seedAdmin;

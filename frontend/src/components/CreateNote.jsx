0eb9bbcb50fd696fef62f777677d4eebb06ce761
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-07-05 12:21:51 +0530

    Crate Note UI style

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../components/CSS/note.css";
 
 export default function CreateNote() {
   return (
     <div>
       <div className="inner">
         <div className="photo">
-          <img src="" />
+          <img src="https://res.cloudinary.com/waste123/image/upload/v1656833398/RMTS/SE/ngvm9vq9whtyg9wbwjhc.png" />
         </div>
         <div className="user-form">
           <h2>My Notes</h2>
@@ -14,8 +15,8 @@ export default function CreateNote() {
 
             <input type="text-area" placeholder="Description" />
 
-            <div className="action-btn ">
-              <button className="btn primary">Create</button>
+            <div className="action-btn " style={{ marginLeft: "70px" }}>
+              <button className="btn primary">Create Note</button>
             </div>
           </form>
         </div>

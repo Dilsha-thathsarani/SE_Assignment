89ac7cbbd34e1909f2a494123d21bd41911fbe5e
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-07-07 02:06:19 +0530

    get a note  by id

--- backend/controllers/noteCtrl.js
+++ backend/controllers/noteCtrl.js
@@ -1,5 +1,6 @@
 import Note from "../models/note.js";
 
+//insert note to the system
 export const createNote = async (req, res) => {
   const { email, title, note } = req.body;
   console.log(req.body);
@@ -11,4 +12,18 @@ export const createNote = async (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+//get a note
+export const getNoteByID = async (req, res) => {
+  let noteId = req.params.id;
+
+  Note.findById(noteId, (err, notes) => {
+    if (err) {
+      return res.status(400).json({ err });
+    }
+    return res.status(200).json({
+      notes,
+    });
+  });
 };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { createNote } from "../controllers/noteCtrl.js";
+import { createNote, getNoteByID } from "../controllers/noteCtrl.js";
 
 router.post("/createNote", createNote);
+router.get("/getNote/:id", getNoteByID);
 export default router;

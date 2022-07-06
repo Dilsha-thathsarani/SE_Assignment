4aafbe1fa4e6cda3342231e79165a6085676fac8
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-07-07 02:37:41 +0530

    update and delete note

--- backend/controllers/noteCtrl.js
+++ backend/controllers/noteCtrl.js
@@ -27,3 +27,39 @@ export const getNoteByID = async (req, res) => {
     });
   });
 };
+
+//delete a note
+export const deleteNotes = async (req, res) => {
+  let noteId = req.params.id;
+  Note.findByIdAndRemove(noteId).exec((err) => {
+    if (err)
+      return res.status(400).json({
+        msg: "Delete Unsuccessfull",
+      });
+
+    return res.json({
+      msg: "Delete Successfull",
+    });
+  });
+};
+
+//update a note
+export const updateNotes = async (req, res) => {
+  let noteId = req.params.id;
+  const { email, title, note } = req.body;
+  console.log("body ", req.body);
+
+  const updateNote = {
+    email,
+    title,
+    note,
+  };
+
+  await Note.findByIdAndUpdate(noteId, updateNote)
+    .then(() => {
+      res.status(200).send({ status: "Updated" });
+    })
+    .catch((err) => {
+      res.status(500).send({ status: "Error update" });
+    });
+};
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { createNote, getNoteByID } from "../controllers/noteCtrl.js";
+import {
+  createNote,
+  getNoteByID,
+  deleteNotes,
+  updateNotes,
+} from "../controllers/noteCtrl.js";
 
 router.post("/createNote", createNote);
 router.get("/getNote/:id", getNoteByID);
+router.delete("/deleteNote/:id", deleteNotes);
+router.put("/updateNote/:id", updateNotes);
 export default router;
